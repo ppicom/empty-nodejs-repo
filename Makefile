ad71ed58bf1811db6e46e207b4146d7b3c386d85
Pere <perepicomuntaner@gmail.com>
2022-11-04 21:21:47 +0100

    Nodejs boilerplate

---
+++ Makefile
@@ -0,0 +1,5 @@
+run:
+	node src/index.js
+
+tests:
+	npm run test
---
+++ test/main.spec.js
@@ -0,0 +1,7 @@
+import * as assert from "assert";
+
+describe("Tests", function() {
+    it('should run', function() {
+        assert.equal(1, 1)
+    })
+})
